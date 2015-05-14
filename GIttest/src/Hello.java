a87a4a8bf1ca3fb9e8fb2c0174746f1c38b3ad67
Hemali90 <hemali.waghode@gmail.com>
2015-05-14 10:48:42 -0700

    line 5 added hemali

--- GIttest/src/Hello.java
+++ GIttest/src/Hello.java
@@ -2,7 +2,7 @@
 public class Hello {
 public static void main(String[] args){
 	System.out.println("Hello");
-	
+	System.out.println("Hemali added");
 	System.out.println("Nisha");
 	System.out.println("Madhu here!");
 	System.out.println("Madhu added again!");
